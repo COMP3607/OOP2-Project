21e56d499a2f9f86b8ebe9993aedb7fd1d2e164a
Brizzy14 <157634819+Brizzy14@users.noreply.github.com>
2025-11-26 14:36:45 +0000

    .

---
+++ src/main/java/org/example/Commands/AnswerQuestionCommand.java
@@ -0,0 +1,15 @@
+package org.example.Commands;
+
+import org.example.Question.JeopardyQuestion;
+import org.example.Question.Option;
+
+public class AnswerQuestionCommand implements Command{
+    private JeopardyQuestion question;
+    private Option option;
+
+
+  public AnswerQuestionCommand(JeopardyQuestion question, Option option){
+    
+  }
+  
+}
